6c90d68611fb43815b699cb9a1ec8d91da957d0b
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-11-28 13:40:03 +0100

    feat: .env

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Home = () => {
+  const apiKey = import.meta.env.VITE_APIKEY;
+
+  console.log(apiKey);
   return <div className="bg-blue-300">Home Page</div>;
 };
 
